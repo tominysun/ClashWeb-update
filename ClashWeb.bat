2f613812198a8146631c010351d297e38e67a81c
lzdnico <596049368@qq.com>
2020-03-03 18:00:44 +0800

    add protable python

--- ClashWeb.bat
+++ ClashWeb.bat
@@ -1,4 +1,5 @@
 @echo off &title Clash Bat
 ::CODER BY pcysanji 2020-02-24
-SET PATH="%~dp0";"%~dp0App";%PATH%
+SET PATH="%~dp0";"%~dp0App";"%~dp0Python";"%~dp0Python\Scripts";"%~dp0Python\Lib\distutils\command";"%~dp0Python\Lib\site-packages\pip\_vendor\distlib";"%~dp0Python\Lib\site-packages\setuptools";%PATH%
 python api.py
+pause

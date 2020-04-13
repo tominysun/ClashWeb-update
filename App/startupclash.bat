e77d355d5a3373d923dbc23d5b6850ee3af454cb
lzdnico <596049368@qq.com>
2020-04-13 09:22:37 +0800

    fix the repeated running of clash core  when booting

--- App/startupclash.bat
+++ App/startupclash.bat
@@ -5,5 +5,3 @@ CD /D %~dp0\..\
 PATH="%CD%\";"%CD%\App";"%CD%\Python";"%CD%\Python\Scripts";"%CD%\Python\Lib\distutils\command";"%CD%\Python\Lib\site-packages\pip\_vendor\distlib";"%CD%\Python\Lib\site-packages\setuptools";%PATH%
 taskkill /IM clash-win64.exe >NUL 2>NUL
 start ClashWeb.exe
-sysproxy set 1
-python node.py startandset

1dc3692dd710b49feaee9a7143862c6797e3aff3
lzdnico <596049368@qq.com>
2020-11-20 11:17:00 +0800

    简化python

--- Python/安装依赖.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d %~dp0
-
-SET PATH="%~dp0";"%~dp0Scripts";"%~dp0\Lib\distutils\command";"%~dp0Lib\site-packages\pip\_vendor\distlib";"%~dp0Lib\site-packages\setuptools";%PATH%
-
-python -m pip3 install -I -r requirements.txt
-pause

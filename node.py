f2c6882896f7810cbeafe00e785b054b0a991dba
lzdnico <596049368@qq.com>
2021-02-08 12:34:47 +0800

    修复tun模式启动失败

--- node.py
+++ node.py
@@ -97,32 +97,32 @@ if __name__ == '__main__':
                     print(s)
                     p=subprocess.Popen(mypath+'/bat/startclasherro.bat '+s,shell=False)
             else:
-                currentconfig = api.admin.getfile('./App/tmp.vbs')                      #获取当前文件
-                currentconfig = str(currentconfig).split('-f')[1].split('\"')[0].replace(' ','').replace('.\\Profile\\','')
+                    currentconfig = api.admin.getfile('./App/tmp.vbs')                      #获取当前文件
+                    currentconfig = str(currentconfig).split('-f')[1].split('\"')[0].replace(' ','').replace('.\\Profile\\','')
 
-                #把普通配置重写为tap配置
-                tapconfig=api.admin.getfile('./Profile/defaultconfig/tapconfig.txt')
-                config=api.admin.getfile('./Profile/'+currentconfig)
-                config=tapconfig+'\nproxies:'+config.split('proxies:',1)[1]       
-                api.admin.writefile(config,'./Profile/tapconfig/'+currentconfig)         
-                #重写完成
+                    #把普通配置重写为tun配置
+                    tapconfig=api.admin.getfile('./Profile/defaultconfig/tunconfig.yaml')
+                    config=api.admin.getfile('./Profile/'+currentconfig)
+                    config=tapconfig+'\nproxies:'+config.split('proxies:',1)[1]       
+                    api.admin.writefile(config,'./Profile/tunconfig/'+currentconfig)         
+                    #重写完成
 
-                p=subprocess.Popen(mypath+'/bat/tapstart.bat',shell=False)              #启动默认tap文件    
-                p.wait() 
-                path=mypath+'/Profile/tapconfig/'+currentconfig
-                path=path.replace('/','\\')
-                p=requests.put(clashapi+'configs',data=json.dumps({'path':path}))       #切换成默认文件
-                print(p.text)  
-                if '' == p.text:  
-                    print(gpus)                     
-                    api.clashapi.setproxies('./Profile/save/'+currentconfig.replace('.yaml','.txt'))       #设置节点     
-                else:
-                    s=p.text.split('\":\"')[1][0:-3]
-                    s='\"'+"启动失败："+s+'\"'
-                    print(s)
-                    p=subprocess.Popen(mypath+'/bat/startclasherro.bat '+s,shell=False) 
-                p=subprocess.Popen(mypath+'/bat/dissys.bat',shell=False)
-                p.wait()  
+                    p=subprocess.Popen(mypath+'/bat/tunstart.bat',shell=False)              #启动默认tun文件    
+                    p.wait() 
+                    path=mypath+'/Profile/tunconfig/'+currentconfig
+                    path=path.replace('/','\\')
+                    p=requests.put(clashapi+'configs',data=json.dumps({'path':path}))       #切换成tun模式配置文件
+                    print(p.text)  
+                    if '' == p.text:  
+                        print(gpus)                     
+                        api.clashapi.setproxies('./Profile/save/'+currentconfig.replace('.yaml','.txt'))       #设置节点  
+                    else:
+                        s=p.text.split('\":\"')[1][0:-3]
+                        s='\"'+"启动失败："+s+'\"'
+                        print(s)
+                        p=subprocess.Popen(mypath+'/bat/startclasherro.bat '+s,shell=False)    
+                    p=subprocess.Popen(mypath+'/bat/dissys.bat',shell=False)
+                    p.wait()  
         except Exception as e:
             pass
 
